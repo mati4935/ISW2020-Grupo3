e4bd33ddb5951f10b00c40a31afd24981ea0562a
Matias Sanchez <matigonsan4935@gmail.com>
2020-09-02 11:24:46 -0300

    modulo

--- Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/app.module.ts
+++ Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioPedidoComponent } from './inicio-pedido/inicio-pedido.component';
+import { EsperaPedidoComponent } from './espera-pedido/espera-pedido.component';
+import { FinPedidoComponent } from './fin-pedido/fin-pedido.component';
+import { ConfirmacionPedidoComponent } from './confirmacion-pedido/confirmacion-pedido.component';
+import { PedidoComponent } from './pedido/pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioPedidoComponent,
+    EsperaPedidoComponent,
+    FinPedidoComponent,
+    ConfirmacionPedidoComponent,
+    PedidoComponent
   ],
   imports: [
     BrowserModule,
