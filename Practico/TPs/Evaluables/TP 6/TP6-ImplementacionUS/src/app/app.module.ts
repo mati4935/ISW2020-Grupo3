e8b45757cd622fec00fcbc82d853ea0c61926c74
Fernando Cisneros Rojo <fercr0803@gmail.com>
2020-09-03 19:43:14 -0300

    nav var agregada

--- Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/app.module.ts
+++ Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PedidoComponent } from './pedido/pedido.component';
+import { MenuComponent } from './menu/menu.component';
+import { APP_BASE_HREF} from '@angular/common';  
 
 @NgModule({
   declarations: [
     AppComponent,
-    PedidoComponent
+    PedidoComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/pedido', pathMatch: 'full' },
+      { path: 'pedido', component: PedidoComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
