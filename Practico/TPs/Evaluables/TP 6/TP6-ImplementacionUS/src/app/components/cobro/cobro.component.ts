35f5b0fa3fe0b8a10dbd9d9ac2749f15ace02c92
Mateo Reyes <mateo.nicolas.reyes@gmail.com>
2020-09-03 23:33:41 -0300

    update cobro

--- Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/components/cobro/cobro.component.ts
+++ Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/components/cobro/cobro.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CobroComponent implements OnInit {
 
+  desplegarEfectivo = "NO";
+  desplegarVISA = "NO";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  ActivarEfectivo()
+  {
+    this.desplegarEfectivo="SI";
+    this.desplegarVISA="NO";
+  }
+  DesactivarEfectivo()
+  {
+    this.desplegarEfectivo="NO";
+  }
+
+  ActivarVISA()
+  {
+    this.desplegarVISA="SI";
+    this.desplegarEfectivo="NO";
+  }
+  DesactivarVISA()
+  {
+    this.desplegarVISA="NO";
   }
 
 }
