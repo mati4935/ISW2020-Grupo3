dac8aae646c2a547370f898a8f467c1e248dc2d4
Matias Sanchez <matigonsan4935@gmail.com>
2020-09-06 20:01:02 -0300

    Estilos y mejoras

--- Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/models/ciudades.ts
+++ Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/models/ciudades.ts
@@ -1,24 +1,10 @@
 export class Ciudad {
-    nombre: string;
-  }
-  
-  export const ciudades: Ciudad[] = [
-    { nombre: "Córdoba" },
-    { nombre: "Rio Ceballos" },
-    { nombre: "Ascochinga" },
-    { nombre: "Saldan" },
-    { nombre: "Unquillo" },
-    { nombre: "Mendiolaza" },
-    { nombre: "Alta Gracia" },
-    { nombre: "Tanti" },
-    { nombre: "Villa General Belgrano" },
-    { nombre: "Santa Rosa de Calamuchita" },
-    { nombre: "Montecristo" },
-    { nombre: "La Falda" },
-    { nombre: "Villa Allende" },
-    { nombre: "Rio Primero" },
-    { nombre: "Rio Cuarto" },
-    { nombre: "La Calera" },
-    { nombre: "Cruz del Eje" },
-    { nombre: "Unquillo" }
-  ]
+  nombre: string;
+}
+
+export const ciudades: Ciudad[] = [
+  { nombre: 'Seleccionar...' },
+  { nombre: 'Córdoba' },
+  { nombre: 'Rio Ceballos' },
+  { nombre: 'Villa Allende' },
+];
