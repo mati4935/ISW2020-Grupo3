8e5feda46f6dd74b029825beba92ab75e71d74dd
matiasgiorda1999 <matiasgiorda16@gmail.com>
2020-09-05 21:55:56 -0300

    Validaciones casi listas

--- Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/components/cobro/cobro.component.ts
+++ Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/components/cobro/cobro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-cobro',
@@ -10,10 +11,20 @@ export class CobroComponent implements OnInit {
   desplegarEfectivo = "NO";
   desplegarVISA = "NO";
   desplegarFechaHs = "NO";
+  formaPago = "NS";
+  recepcion = "NS";
+  formFormaPago:FormGroup;
+  formVISA:FormGroup;
+  formEFECTIVO:FormGroup;
+  formRECEPCION:FormGroup;
+  enviado:boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.formFormaPago = this.formBuilder.group({
+      precio: [null, [Validators.required, Validators.pattern("[0-9]{1,10}")]]
+     })
   }
 
 
@@ -21,6 +32,10 @@ export class CobroComponent implements OnInit {
   {
     this.desplegarEfectivo="SI";
     this.desplegarVISA="NO";
+    this.formaPago="E";
+    this.formEFECTIVO = this.formBuilder.group({
+      monto: [null, [Validators.required, Validators.pattern("[0-9]{1,10}")]]
+     })
   }
   DesactivarEfectivo()
   {
@@ -31,6 +46,14 @@ export class CobroComponent implements OnInit {
   {
     this.desplegarVISA="SI";
     this.desplegarEfectivo="NO";
+    this.formaPago="V";
+    this.formVISA = this.formBuilder.group({
+      nombreTitular: ["",[Validators.required, Validators.minLength(1), Validators.maxLength(55)]],
+      apellidoTitular: ["",[Validators.required, Validators.minLength(1), Validators.maxLength(55)]],
+      numeroTarjeta: [null, [Validators.required, Validators.pattern("[0-9]{16}")]],
+      codigoTarjeta: [null, [Validators.required, Validators.pattern("[0-9]{3}")]],
+     })
+
   }
   DesactivarVISA()
   {
@@ -40,11 +63,55 @@ export class CobroComponent implements OnInit {
   ActivarFechaHs()
   {
     this.desplegarFechaHs="SI";
+    this.recepcion="FyH";
+    this.formRECEPCION = this.formBuilder.group({
+      fecha: ["",[Validators.required,Validators.pattern("(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}")]],
+      hora: [null, [Validators.required, Validators.pattern("[0-2]{1}[0-4]{1}")]],
+     })
   }
 
   DesactivarFechaHs()
   {
     this.desplegarFechaHs="NO";
+    this.recepcion = "AP"
+  }
+  
+  Confirmar()
+  {
+    this.enviado = true;
+    if(this.recepcion == "NS" || this.formaPago == "NS")
+    {
+      return;
+    }
+    if(this.formaPago == "E" && this.recepcion == "AP")
+    {
+      if(this.formFormaPago.invalid || this.formEFECTIVO.invalid )
+      {
+        return;
+      }
+    }
+    if(this.formaPago == "V" && this.recepcion == "AP")
+    {
+      if(this.formFormaPago.invalid || this.formVISA.invalid )
+      {
+        return;
+      }
+    }
+    if(this.formaPago == "E" && this.recepcion == "FyH")
+    {
+      if(this.formFormaPago.invalid || this.formEFECTIVO.invalid || this.formRECEPCION.invalid)
+      {
+        return;
+      }
+    }
+    if(this.formaPago == "V" && this.recepcion == "FyH")
+    {
+      if(this.formFormaPago.invalid || this.formVISA.invalid || this.formRECEPCION.invalid)
+      {
+        return;
+      }
+    }
+    alert("BIEN ECHO")
   }
 
 }
