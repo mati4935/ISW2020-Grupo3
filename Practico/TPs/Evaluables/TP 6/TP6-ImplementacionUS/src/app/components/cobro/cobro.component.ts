b1e8ace1933143d2ab364a27357e01e47ce92bbf
Matias Sanchez <matigonsan4935@gmail.com>
2020-09-06 21:11:50 -0300

    validaciones fecha

--- Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/components/cobro/cobro.component.ts
+++ Practico/TPs/Evaluables/TP 6/TP6-ImplementacionUS/src/app/components/cobro/cobro.component.ts
@@ -1,120 +1,134 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cobro',
   templateUrl: './cobro.component.html',
-  styleUrls: ['./cobro.component.css']
+  styleUrls: ['./cobro.component.css'],
 })
 export class CobroComponent implements OnInit {
+  desplegarEfectivo = 'NO';
+  desplegarVISA = 'NO';
+  desplegarFechaHs = 'NO';
+  formaPago = 'NS';
+  recepcion = 'NS';
+  formFormaPago: FormGroup;
+  formVISA: FormGroup;
+  formEFECTIVO: FormGroup;
+  formRECEPCION: FormGroup;
+  enviado: boolean = false;
 
-  desplegarEfectivo = "NO";
-  desplegarVISA = "NO";
-  desplegarFechaHs = "NO";
-  formaPago = "NS";
-  recepcion = "NS";
-  formFormaPago:FormGroup;
-  formVISA:FormGroup;
-  formEFECTIVO:FormGroup;
-  formRECEPCION:FormGroup;
-  enviado:boolean = false;
-
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.formFormaPago = this.formBuilder.group({
-      precio: [null, [Validators.required, Validators.pattern("[0-9]{1,10}")]]
-     });
+      precio: [null, [Validators.required, Validators.pattern('[0-9]{1,10}')]],
+    });
 
-     this.formVISA = this.formBuilder.group({
-      nombreTitular: ["",[Validators.required, Validators.minLength(1), Validators.maxLength(55)]],
-      apellidoTitular: ["",[Validators.required, Validators.minLength(1), Validators.maxLength(55)]],
-      numeroTarjeta: [null, [Validators.required, Validators.pattern("[0-9]{16}")]],
-      codigoTarjeta: [null, [Validators.required, Validators.pattern("[0-9]{3}")]],
-     });
-     this.formEFECTIVO = this.formBuilder.group({
-      monto: [null, [Validators.required, Validators.pattern("[0-9]{1,10}")]]
-     });
-     
-     this.formRECEPCION = this.formBuilder.group({
-      fecha: ["",[Validators.required,Validators.pattern("(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}")]],
-      hora: [null, [Validators.required, Validators.pattern("([0-2]?[0-9]?)[-:]([0-5]?[0-9]?)")]],
-     })
-  }
+    this.formVISA = this.formBuilder.group({
+      nombreTitular: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(55),
+        ],
+      ],
+      apellidoTitular: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(1),
+          Validators.maxLength(55),
+        ],
+      ],
+      numeroTarjeta: [
+        null,
+        [Validators.required, Validators.pattern('[4]{1}[0-9]{15}')],
+      ],
+      codigoTarjeta: [
+        null,
+        [Validators.required, Validators.pattern('[0-9]{3}')],
+      ],
+    });
+    this.formEFECTIVO = this.formBuilder.group({
+      monto: [null, [Validators.required, Validators.pattern('[0-9]{1,10}')]],
+    });
 
-
-  ActivarEfectivo()
-  {
-    this.desplegarEfectivo="SI";
-    this.desplegarVISA="NO";
-    this.formaPago="E";
-    
-  }
-  DesactivarEfectivo()
-  {
-    this.desplegarEfectivo="NO";
-  }
-
-  ActivarVISA()
-  {
-    this.desplegarVISA="SI";
-    this.desplegarEfectivo="NO";
-    this.formaPago="V";
-  }
-  DesactivarVISA()
-  {
-    this.desplegarVISA="NO";
-  }
-
-  ActivarFechaHs()
-  {
-    this.desplegarFechaHs="SI";
-    this.recepcion="FyH";
-    
+    this.formRECEPCION = this.formBuilder.group({
+      //fecha: ["",[Validators.required,Validators.pattern("(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}")]],
+      fecha: [null],
+      hora: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern('([0-2]?[0-9]?)[-:]([0-5]?[0-9]?)'),
+        ],
+      ],
+    });
   }
 
-  DesactivarFechaHs()
-  {
-    this.desplegarFechaHs="NO";
-    this.recepcion = "AP"
+  ActivarEfectivo() {
+    this.desplegarEfectivo = 'SI';
+    this.desplegarVISA = 'NO';
+    this.formaPago = 'E';
   }
-  
-  Confirmar()
-  {
+  DesactivarEfectivo() {
+    this.desplegarEfectivo = 'NO';
+  }
+
+  ActivarVISA() {
+    this.desplegarVISA = 'SI';
+    this.desplegarEfectivo = 'NO';
+    this.formaPago = 'V';
+  }
+  DesactivarVISA() {
+    this.desplegarVISA = 'NO';
+  }
+
+  ActivarFechaHs() {
+    this.desplegarFechaHs = 'SI';
+    this.recepcion = 'FyH';
+  }
+
+  DesactivarFechaHs() {
+    this.desplegarFechaHs = 'NO';
+    this.recepcion = 'AP';
+  }
+
+  Confirmar() {
     this.enviado = true;
-    if(this.recepcion == "NS" || this.formaPago == "NS")
-    {
+    if (this.recepcion == 'NS' || this.formaPago == 'NS') {
       return;
     }
-    if(this.formaPago == "E" && this.recepcion == "AP")
-    {
-      if(this.formFormaPago.invalid || this.formEFECTIVO.invalid )
-      {
+    if (this.formaPago == 'E' && this.recepcion == 'AP') {
+      if (this.formFormaPago.invalid || this.formEFECTIVO.invalid) {
         return;
       }
     }
-    if(this.formaPago == "V" && this.recepcion == "AP")
-    {
-      if(this.formFormaPago.invalid || this.formVISA.invalid )
-      {
+    if (this.formaPago == 'V' && this.recepcion == 'AP') {
+      if (this.formFormaPago.invalid || this.formVISA.invalid) {
         return;
       }
     }
-    if(this.formaPago == "E" && this.recepcion == "FyH")
-    {
-      if(this.formFormaPago.invalid || this.formEFECTIVO.invalid || this.formRECEPCION.invalid)
-      {
+    if (this.formaPago == 'E' && this.recepcion == 'FyH') {
+      if (
+        this.formFormaPago.invalid ||
+        this.formEFECTIVO.invalid ||
+        this.formRECEPCION.invalid
+      ) {
         return;
       }
     }
-    if(this.formaPago == "V" && this.recepcion == "FyH")
-    {
-      if(this.formFormaPago.invalid || this.formVISA.invalid || this.formRECEPCION.invalid)
-      {
+    if (this.formaPago == 'V' && this.recepcion == 'FyH') {
+      if (
+        this.formFormaPago.invalid ||
+        this.formVISA.invalid ||
+        this.formRECEPCION.invalid
+      ) {
         return;
       }
     }
-    alert("BIEN ECHO")
+    alert('BIEN ECHO');
   }
-
 }
